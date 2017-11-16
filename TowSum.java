373d81182ad41b22fdf79a4583eb8949710bd84c
xmfaly <xmfaly@gmail.com>
2017-11-16 15:37:28 +0800

    补充towsum的一种解法

--- TowSum.java
+++ TowSum.java
@@ -1,21 +1,46 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * 直接遍历即可
  */
+//public class TowSum {
+//
+//    class Solution {
+//        public int[] twoSum(int[] nums, int target) {
+//
+//            int[] ans = new int[2];
+//            for(int i=0;i<nums.length;i++){
+//                for(int j=i+1;j<nums.length;j++){
+//                    if (nums[i] + nums[j] == target){
+//                        ans[0] = i;
+//                        ans[1] = j;
+//                    }
+//                }
+//            }
+//            return ans;
+//        }
+//    }
+//}
+
 public class TowSum {
 
     class Solution {
         public int[] twoSum(int[] nums, int target) {
-
-            int[] ans = new int[2];
-            for(int i=0;i<nums.length;i++){
-                for(int j=i+1;j<nums.length;j++){
-                    if (nums[i] + nums[j] == target){
-                        ans[0] = i;
-                        ans[1] = j;
-                    }
+            int[] res = new int[2];
+            if (nums == null || nums.length < 2)
+                return res;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (!map.containsKey(target - nums[i])) {
+                    map.put(nums[i], i);
+                } else {
+                    res[0] = map.get(target - nums[i]);
+                    res[1] = i;
+                    break;
                 }
             }
-            return ans;
+            return res;
         }
     }
 }
